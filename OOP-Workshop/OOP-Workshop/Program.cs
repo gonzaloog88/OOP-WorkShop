84a81fcfc1ceb15e273b731b34f109a3c874f90f
gonzaloog88 <gonzaloog88@gmail.com>
2022-03-26 23:54:42 -0600

    VariablePriceProduct class, finished. Creation of VariablePriceProduct class objects in the main program.

--- OOP-Workshop/OOP-Workshop/Program.cs
+++ OOP-Workshop/OOP-Workshop/Program.cs
@@ -19,5 +19,27 @@ Product product2 = new FixedPriceProduct()
    Tax = 0.19F
 };
 
+Product product3 = new VariablePriceProduct()
+{
+   Description = "Queso Holandes",
+   Id = 3030,
+   Measurement = "Kilo",
+   Price = 32000M,
+   Quantity = 0.536F,
+   Tax = 0.19F
+};
+
+Product product4 = new VariablePriceProduct()
+{
+   Description = "Cabano",
+   Id = 4040,
+   Measurement = "Kilo",
+   Price = 18000M,
+   Quantity = 0.389F,
+   Tax = 0.19F
+};
+
 Console.WriteLine(product1);
 Console.WriteLine(product2);
+Console.WriteLine(product3);
+Console.WriteLine(product4);
---
+++ OOP-Workshop/OOP-Workshop/VariablePriceProduct.cs
@@ -0,0 +1,23 @@
+﻿namespace OOP_Workshop
+{
+   public class VariablePriceProduct : Product
+   {
+      public string Measurement { get; set; }
+      public float Quantity { get; set; }
+
+      public override decimal ValueToPay()
+      {
+         return ((decimal)Quantity * Price) * ((decimal)Tax + 1);
+      }
+
+      public override string ToString()
+      {
+         return $"  {Id} {Description}" +
+            $"\n\tMeasurement.............:{Measurement,20}" +
+            $"\n\tQuantity................:{Quantity,20:N2}" +
+            $"\n\tPrice...................:{Price,20:C2}" +
+            $"\n\tTax.....................:{Tax,20:P2}" +
+            $"\n\tValue...................:{ValueToPay(),20:C2}";
+      }
+   }
+}
