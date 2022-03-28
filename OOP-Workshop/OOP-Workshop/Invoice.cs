25ee57183e9f44f609e27ad3f394f4e42b5fa73f
gonzaloog88 <gonzaloog88@gmail.com>
2022-03-27 18:34:55 -0600

    Invoice class, finished. Creation of Invoice object in the main program. Project finished.

---
+++ OOP-Workshop/OOP-Workshop/Invoice.cs
@@ -0,0 +1,42 @@
+﻿namespace OOP_Workshop
+{
+   public class Invoice : IPay
+   {
+      private ICollection<Product> _products;
+
+      public Invoice()
+      {
+         _products = new List<Product>();
+      }
+
+      public void AddProduct(Product product)
+      {
+         _products.Add(product);
+      }
+
+      public decimal ValueToPay()
+      {
+         decimal valueToPay = 0M;
+         foreach (Product product in _products)
+         {
+            valueToPay += product.ValueToPay();
+         }
+         return valueToPay;
+      }
+
+      public override string ToString()
+      {
+         string products = string.Empty;
+         foreach (Product product in _products)
+         {
+            products += $"{product}\n";
+         }
+         return $"------------------------------------------------------------" +
+            $"\n                          RECEIPT" +
+            $"\n------------------------------------------------------------" +
+            $"\n{products}" +
+            $"                                 =====================" +
+            $"\nTOTAL                             {ValueToPay(),20:C2}";
+      }
+   }
+}
--- OOP-Workshop/OOP-Workshop/Program.cs
+++ OOP-Workshop/OOP-Workshop/Program.cs
@@ -52,3 +52,9 @@ Console.WriteLine(product2);
 Console.WriteLine(product3);
 Console.WriteLine(product4);
 Console.WriteLine(product5);
+
+Invoice invoice = new Invoice();
+invoice.AddProduct(product1);
+invoice.AddProduct(product3);
+invoice.AddProduct(product5);
+Console.WriteLine(invoice);
